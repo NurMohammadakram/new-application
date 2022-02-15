266d63a0cf97bcd93bed0474044e9aaed3c12630
NurMohammadakram <nurmohammad19k8@gmail.com>
2022-02-16 04:50:02 +0600

    okk

--- src/Component/Nav_secton/Navbar.js
+++ src/Component/Nav_secton/Navbar.js
@@ -3,9 +3,9 @@ import './Navbar.css';
 import logo from '../../asset/images/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Navbar(props) {
-
 
     const handler = (event) => {
         event.preventDefault()
@@ -20,12 +20,11 @@ function Navbar(props) {
             </div>
             
             <ul className='navbar-nav'>
-                <li className='list'><a href="/home">Home</a></li>
-                <li className='list'><a href="/shop">Shop</a></li>
-                <li className='list'><a href="/account">My Account</a></li>
-                <li className='list'><a href="/cart"><FontAwesomeIcon icon={faShoppingCart} />Cart</a></li>
+                <Link className='nav-link' to='shop'>Shop</Link>
+                <Link className='nav-link' to='account'>My Account</Link>
+                <Link className='nav-link' to='cart'><FontAwesomeIcon icon={faShoppingCart} />Cart</Link>
             </ul>
-        </nav>    
+        </nav>
     );
 }
 
